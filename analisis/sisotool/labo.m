32f6b6e0c1f18b9d610478fe72a0fc9418475d24
Paulo Loma Marconi <zurits@gmail.com>
2018-04-05 19:59:34 -0400

    .

--- analisis/sisotool/labo.m
+++
@@ -1,9 +0,0 @@
-%Gp de 1er orden
-Kp=0.95129; Tp1=0.10921;
-Gp1=tf([Kp],[Tp1 1])
-b=Kp/Tp1
-a=1/Tp1
-%Gp de 2do orden
-Kp=0.93548; Tp1=0.051052; Tp2=0.05105;
-Gp2=tf([Kp],[(Tp1*Tp2) (Tp1+Tp2) 1])
-step(Gp1,Gp2)
